219a7fad01dbf204a677bc904a61e0b8b4577bc9
Munaf29 <mkkazi54321@gmail.com>
2024-07-24 18:06:04 +0000

    const , let and var

---
+++ 02_const_let_var/variables.js
@@ -0,0 +1,10 @@
+// let const and var are keywords they are used to define  variables.
+// var is global scoped
+// let and const keywords are block scoped 
+// to change and value in let you need to redeclare the let keywords
+
+const myName = "Munaf";
+let myAge = 21;
+var myBirthdate = 11-12-2000;
+
+console.table([myName, myAge, myBirthdate])
